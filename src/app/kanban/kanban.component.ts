afe7297de4c186c647c9405c932b2cc92cca81cd
breia <breiaswanigan1@gmail.com>
2020-09-15 15:26:03 -0400

    kanban board function and styling

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban',
   templateUrl: './kanban.component.html',
-  styleUrls: ['./kanban.component.css']
+  styleUrls: ['./kanban.component.css'],
 })
 export class KanbanComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'v7-dragdrop';
+  numbers: number[] = [];
+  otherNumbers: number[] = [];
+  doneNumbers: number[] = [];
+  constructor() {
+    for (let i = 0; i < 11; i++) {
+      this.numbers.push(i);
+    }
   }
-
+  drop(event: CdkDragDrop<number[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+  ngOnInit(): void {}
 }

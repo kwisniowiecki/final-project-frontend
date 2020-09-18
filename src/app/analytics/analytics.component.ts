dbd8715500ef8da457e72049d0924656c7e391b3
0sorojo <redrootproductions@gmail.com>
2020-09-18 18:56:13 -0400

    got chart colors to match

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -9,8 +9,15 @@ import { BackpackService } from '../backpack.service';
 })
 export class AnalyticsComponent implements OnInit {
   // adventures: any = [];
-  dailySubject: any = [];
-  overallSubject: any = [];
+  labels: string[] = [
+    'Math',
+    'Misc',
+    'Reading',
+    'Science',
+    'Social Studies',
+    'Writing',
+    'Incomplete',
+  ];
   dailyIncomplete: any;
   totalIncomplete: any;
   dailyChartData: any = [];
@@ -38,34 +45,32 @@ export class AnalyticsComponent implements OnInit {
   ngOnInit(): void {
     // this.getAdventures();
     this.getDate();
-    this.getDailyComplete();
-    this.getDailyIncomplete();
-    this.getComplete();
-    this.getIncomplete();
-  }
 
-  // getAdventures = () => {
-  //   this.service.getAdventures().subscribe((response) => {
-  //     this.adventures = response;
-  //     console.log(this.adventures);
-  //   });
-  // };
+    this.getDailyComplete();
+
+    this.getComplete();
+  }
 
   getDate = () => {
     this.todaysDate = this.service.getDate();
     console.log(this.todaysDate);
   };
 
-  // getDailyComplete
   getDailyComplete = () => {
     this.service.getDailyComplete().subscribe((response) => {
+      console.log(response);
       this.dailyCompleteArray = response;
-      console.log(this.dailyCompleteArray);
-      this.dailyCompleteArray.forEach((item) => {
-        this.dailySubject.push(item.subject);
-        this.dailyChartData.push(item.count);
+      this.labels.forEach((label) => {
+        let found = this.dailyCompleteArray.find((item) => {
+          return item.subject === label;
+        });
+        if (found) {
+          this.dailyChartData.push(found.count);
+        } else {
+          this.dailyChartData.push(0);
+        }
       });
-      console.log(this.dailyChartData, this.dailySubject);
+      this.getDailyIncomplete();
     });
   };
 
@@ -74,10 +79,9 @@ export class AnalyticsComponent implements OnInit {
     this.service.getComplete().subscribe((response) => {
       this.completeArray = response;
       this.completeArray.forEach((item) => {
-        this.overallSubject.push(item.subject);
         this.overallChartData.push(item.count);
       });
-      console.log(this.overallChartData, this.overallSubject);
+      this.getIncomplete();
     });
   };
 
@@ -85,8 +89,8 @@ export class AnalyticsComponent implements OnInit {
   getDailyIncomplete = () => {
     this.service.getDailyIncomplete().subscribe((response) => {
       this.dailyIncomplete = response[0].count;
-      this.dailyChartData.push(this.dailyIncomplete);
-      this.dailySubject.push('Incomplete');
+      this.dailyChartData[6] = this.dailyIncomplete;
+      console.log(response);
     });
   };
 
@@ -95,7 +99,6 @@ export class AnalyticsComponent implements OnInit {
     this.service.getIncomplete().subscribe((response) => {
       this.totalIncomplete = response[0].total;
       this.overallChartData.push(this.totalIncomplete);
-      this.overallSubject.push('Incomplete');
     });
   };
 }

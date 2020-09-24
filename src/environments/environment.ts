56fbd13e3eb3f66e40e392fe4ab791735b906451
0sorojo <redrootproductions@gmail.com>
2020-09-24 15:23:49 -0400

    runthrough

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
-  // apiBaseUrl: 'https://backpack4kids.herokuapp.com',
+  // apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'https://backpack4kids.herokuapp.com',
 };
 
 /*
